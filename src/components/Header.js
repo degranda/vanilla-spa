14c598d9e030b43d87c1027dde0f0211d221539b
Diego De Granda <degranda@gmail.com>
2020-02-15 08:46:12 -0600

    first commit vanilla spa

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+    const view = `
+     <div class="header-logo">
+        <h1>
+            <a href="/">100tifi.co</a>
+        </h1>
+     </div>
+     <div>
+        <a href="#/about/">About</a>
+     </div>
+    `; 
+    return view;
+}
+
+export default Header;
---
+++ src/pages/Error404.js
@@ -0,0 +1,11 @@
+const Error404 = () => {
+    const view = `
+        <div class="error404">
+            <h1>Lo siento, la página que buscas, no está :S</h1>
+        </div>
+    `;
+
+    return view;
+}
+
+export default Error404;
